07a9d7a2f9e689e78e068da63faa4760e90c34aa
adityak5212 <adityak5212@gmail.com>
2026-03-12 11:36:18 +0530

    Shopping cart program

--- main.java
+++ main.java
@@ -1,35 +1,40 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
-public class main{
-    public static void main(String[] args){
 
-        // MAD LIBS GAME
+public class main {
+    public static void main (String[] args) {
+
+        // SHOPPING CART PROGRAM
 
         Scanner scanner = new Scanner(System.in);
 
-        String adjective1;
-        String noun1;
-        String adjective2;
-        String verb1;
-        String adjective3;
+        String item;
+        double price;
+        int quantity;
+        char currency ='$';
+        double total;
 
-        System.out.print("Enter an adjective (description): ");
-        adjective1 = scanner.nextLine();
-        System.out.print("Enter a noun (animal or person): ");
-        noun1 = scanner.nextLine();
-        System.out.print("Enter an adjective adjective (description): ");
-        adjective2 = scanner.nextLine();
-        System.out.print("Enter a verb end with -ing (action): ");
-        verb1 = scanner.nextLine();
-        System.out.print("Enter an adjective (description): ");
-        adjective3 = scanner.nextLine();
+        System.out.print("What item would you like to buy? ");
+        item = scanner.nextLine();
 
-        System.out.println("\nToday i went to a " + adjective1 + " zoo. ");
-        System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
-        System.out.println("I was " + adjective3 + "!");
+        System.out.print("What is price of each? ");
+        price = scanner.nextDouble();
+
+        System.out.print("How many would you like? ");
+        quantity = scanner.nextInt();
+
+        total = price * quantity;
+
+        System.out.println("\nYou have bought " + quantity + " " + item +"/s");
+        System.out.println("Your total is " + currency + total);
+
 
         scanner.close();
+
+
+
+
+
+
 
     }
 }
